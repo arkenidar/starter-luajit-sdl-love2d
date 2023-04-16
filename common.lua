4413f5c6eac044c30963a8e577420ffc69481f50
Dario Cangialosi <dario.cangialosi@gmail.com>
2023-04-16 21:41:26 +0200

    main note: anim_x = -anim_width+(anim_x%screen_width) means that starts from anim_x = -anim_width so just enters into view from left, beginning with just the first right part (and not: starting with the whole rectangle already into view from every periodic beginning). additional note: renaming happened for clarity improvement. next: starter uses -mwindows luajit in next commit.

--- common.lua
+++ common.lua
@@ -2,33 +2,36 @@
 -- common API is: update, draw, draw_rectangle
 
 -- animation 1
-local intensity = 1
-local angle= 90
+local pulsation_intensity = 1
+local pulsation_angle= 90
 
 -- animation 2
-local x = 0
+local anim_x = 0
+local anim_width = 50
+local screen_width = 300
 
 -- https://love2d.org/wiki/love.update
 function update(dt)
   
   -- animation 1
   local increment = 45*dt -- degrees
-  angle = angle + increment
-  while angle>360 do angle=angle-360 end -- circle
-  local radiants = angle/360*math.pi*2
-  intensity= math.abs(math.sin(radiants))
+  pulsation_angle = pulsation_angle + increment
+  while pulsation_angle>360 do pulsation_angle=pulsation_angle-360 end -- circle
+  local radiants = pulsation_angle/360*math.pi*2
+  pulsation_intensity= math.abs(math.sin(radiants))
   
   -- animation 2
   local increment_x = 100*dt
-  x = ( x + increment_x ) % 300
+  anim_x = anim_x + increment_x
+  if anim_x > screen_width then anim_x = -anim_width+(anim_x%screen_width) end
 end
 
 -- https://love2d.org/wiki/love.draw
 function draw()
   
   -- animation 1
-  draw_rectangle({intensity,1-intensity,0}, {10+40*intensity,50,200,100})
+  draw_rectangle({pulsation_intensity,1-pulsation_intensity,0}, {10+40*pulsation_intensity,50,200,100})
   
   -- animation 2
-  draw_rectangle({1,1,0}, {x,250,50,5})
+  draw_rectangle({1,1,0}, {anim_x,250,anim_width,5})
 end
