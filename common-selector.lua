a108da51f03e1c24824ea2cf1d799734c558ffc3
Dario Cangialosi a.k.a. Arkenidar <dario.cangialosi@gmail.com>
2024-04-11 21:34:23 +0200

    require(arg[1]) for loading apps via arg

--- common-selector.lua
+++ common-selector.lua
@@ -1,6 +1,12 @@
 
 -- select which?
 
+local common_file_name = arg[1] -- require(arg[1])
+
+--print(common_file_name)
+
+if common_file_name then require(common_file_name) ; return end
+
 ---require('common') -- first applied example for this codebase
 
 require('common-gui-1') -- gui example from: https://github.com/arkenidar/lua-love2d/blob/main/grab-move2/main.lua
